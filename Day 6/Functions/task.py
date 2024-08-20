d6880647e08ed4f9c90d04a4e20b80bbe09e6b17
Laxsan Jeya <l.jeya86@gmail.com>
2024-08-19 23:24:38 -0400

    day 6 Maze functions

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -1,37 +1,30 @@
+'''
+Lost in a maze
+Reeborg was exploring a dark maze and the battery in its flashlight ran out.
 
-def get_user_name():
-    name = input("What is your name? ")
-    print("hello")
-    print(name)
-    return name
-    # Inside the function
+Write a program using an if/elif/else statement so Reeborg can find the exit. The secret is to have Reeborg follow along the right edge of the maze, turning right if it can, going straight ahead if it can’t turn right, or turning left as a last resort.
 
-#Outside the function
-print("Hello")
-get_user_name() # Calling the function
+What you need to know
+The functions move() and turn_left().
+Either the test front_is_clear() or wall_in_front(), right_is_clear() or wall_on_right(), and at_goal().
+How to use a while loop and if/elif/else statements.
+It might be useful to know how to use the negation of a test (not in Python).
 
-print("-----------------------------------")
 
-def get_user_name2():
-    name2 = input("What is your name? ")
-    return name2
-    # Inside the function
+'''
 
-#Outside the function
-print("Hello")
-name2 = get_user_name2() # Calling the function
-print("hello")
-print(name2)
-
-print("-----------------------------------")
-
-name3 = ""
-def get_user_name3():
-    name3 = input("What is your name? ")
-    # Inside the function
-
-#Outside the function
-print("Hello")
-get_user_name3() # Calling the function
-print("Hello")
-print(name3)
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while front_is_clear():
+    move()
+turn_left()
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    elif not front_is_clear():
+        turn_left()
