5331d34d63deb76cecd3fc8a5321c98a46d9a3d4
Laxsan Jeya <l.jeya86@gmail.com>
2024-08-25 01:06:44 -0400

    day 9 Dictionaries and Nested Lists

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,30 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+from art import logo
 
+print(logo)
+taken_bids = {}
+take_bids = True
 
+while take_bids:
+    name = input("what is your name?")
+    bid = int(input("what is your bid"))
+    taken_bids[name] = bid
+    others = input("are there other bidders?\nPlease answer 'y' for yes.")
+    print("\n" * 105)
+    if others == "y":
+        take_bids = True
+    else:
+        take_bids = False
+
+def find_highest_bid():
+    highest_bid = 0
+    highest_bidder = ""
+
+    for key in taken_bids:
+        if taken_bids[key] > highest_bid:
+            highest_bid = taken_bids[key]
+            highest_bidder = key
+
+    print(f"the highest bid was {highest_bid}")
+    print(f"the highest bidder was {highest_bidder} with ${highest_bid} bid")
+
+find_highest_bid()
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,8 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again.",
+                          "Loop": "Something that repeats itself",
+                          "Internet": "Surfing the web"
+                          }
+
+for thing in programming_dictionary:
+    print(f"{thing} : {programming_dictionary[thing]}")
